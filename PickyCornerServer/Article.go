103f76667a1e5e8cd4ad5bd21593542e802266c9
manish181192 <manishvidyasagar@gmail.com>
2022-12-04 20:42:08 -0800

    Added structs for author and article tables

---
+++ PickyCornerServer/Article.go
@@ -0,0 +1,10 @@
+package main
+
+type Article struct {
+	authorId    int
+	articleId   int
+	tags        []string
+	dateCreated string
+	totalViews  int
+	imageIds    []int
+}
---
+++ PickyCornerServer/Author.go
@@ -0,0 +1,10 @@
+package main
+
+type Author struct {
+	authorId   int
+	name       string
+	signupDate string
+	authorTags []string
+	country    string
+	phone      string
+}
